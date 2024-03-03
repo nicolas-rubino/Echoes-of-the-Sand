34174623b016bf367c4d5abf70577abadbb14761
nicolas_rubino <49950602+nicolas-rubino@users.noreply.github.com>
2024-03-02 20:14:54 -0600

    Compass Bar

---
+++ Echoes of the Sand/Assets/Script/UI/Compass_Bar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass_Bar : MonoBehaviour
+{
+    public RectTransform compassBar;
+
+    public RectTransform objectiveMarkerTransform;
+    public RectTransform northMarkerTransform;
+    public RectTransform southMarkerTransform;
+
+    public Transform cameraTransform;
+    public Transform objectiveTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetMarkerPosition(objectiveMarkerTransform, objectiveTransform.position);
+        SetMarkerPosition(northMarkerTransform, Vector3.forward * 1000);
+        SetMarkerPosition(southMarkerTransform, Vector3.back * 1000);
+
+    }
+
+    private void SetMarkerPosition(RectTransform marker, Vector3 worldPosition)
+    {
+        Vector3 directionToTarget = worldPosition - cameraTransform.position;
+        float angle = Vector2.SignedAngle(new Vector2(directionToTarget.x, directionToTarget.z),
+            new Vector2(cameraTransform.transform.forward.x, cameraTransform.transform.forward.z));
+        float compassPositionX = Mathf.Clamp(2 * angle / Camera.main.fieldOfView, -1, 1);
+
+        marker.anchoredPosition = new Vector2(compassBar.rect.width / 2 * compassPositionX, 0);
+    
+    }
+}
